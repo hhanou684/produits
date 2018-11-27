e59add9796b5f0a690846e81d4cd1a5a48e837a9
hana baraket <hana baraket@hanabaraket>
2018-11-27 15:39:50 +0100

    hi

---
+++ ProduitServiceMongoDB/src/main/java/com/example/demo/ProduitServiceMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitServiceMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitServiceMongoDbApplication.class, args);
+	}
+}
